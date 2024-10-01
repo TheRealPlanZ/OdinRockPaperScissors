0afccc964a04f37bf3c0b5d1e3efdf8917565046
Zack Levin <112544104+TheRealPlanZ@users.noreply.github.com>
2024-10-01 16:16:46 -0700

    HTML and pseudo
    Made HTML and the steps of project as pseudo code

---
+++ script.js
@@ -0,0 +1,9 @@
+// Step 2: Function to get computer's choice
+
+// Step 3: Function to get human's choice
+
+// Step 4: Declare score variables
+
+// Step 5: Function to play a round
+
+// Step 6: Function to play entire game
